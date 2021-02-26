33dab8858b057a78e7074a0c88881c44408920c8
Gabriel Ruiz <70815190+gabrielmruiz@users.noreply.github.com>
2021-02-26 16:02:42 -0300

    Update HorarioLancamento.java

--- HorarioLancamento.java
+++ HorarioLancamento.java
@@ -1,8 +1,14 @@
 package Projeto;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class HorarioLancamento {
-
+	
+	public static void main(String args[]) {
+		
 	LocalDateTime now = LocalDateTime.now();
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/YYYY  h:mm a");
+	System.out.println(dtf.format(now));
+	}	
 }
